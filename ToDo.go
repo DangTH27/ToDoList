e8fd3f0c1788d45aada0cd3161a49a14432593e1
Hải Đăng <152187325+DangTH27@users.noreply.github.com>
2024-01-21 05:07:13 +0700

    Update ToDo.go

--- ToDo.go
+++ ToDo.go
@@ -14,7 +14,6 @@ import (
 type TodoItem struct {
 	Id          int        `json:"id" gorm:"column:id;"`
 	Title       string     `json:"title" gorm:"column:title;"`
-	Description string     `json:"description" gorm:"column:description;"`
 	Status      string     `json:"status" gorm:"column:status;"`
 	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at;"`
 	UpdatedAt   *time.Time `json:"updated_at" gorm:"column:updated_at;"`
@@ -25,14 +24,12 @@ func (TodoItem) TableName() string { return "todo_items" }
 type TodoItemCreation struct {
 	Id          int    `json:"-" gorm:"column:id;"`
 	Title       string `json:"title" gorm:"column:title;"`
-	Description string `json:"description" gorm:"column:description;"`
 }
 
 func (TodoItemCreation) TableName() string { return "todo_items" }
 
 type TodoItemUpdate struct {
 	Title       *string `json:"title" gorm:"column:title;"`
-	Description *string `json:"description" gorm:"column:description;"`
 	Status      *string `json:"status" gorm:"column:status;"`
 }
 
